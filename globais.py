3026c9e40e8babdc98802702533d22b21d32740b
Gabriel Dertoni <gab.dertoni@gmail.com>
2020-10-04 01:10:37 -0300

    Variaveis globais mudadas de volta para as originais

--- globais.py
+++ globais.py
@@ -1,7 +1,10 @@
-grupo_teste = [876005334]
-mat_discreta = []
-icc = []
-fisica = []
-algoritmos = []
-grupos_sala = []
-grupos_permitidos = [grupo_teste[0]]
+from helpers import loadFile
+
+IDS = loadFile("Ignore/Groups.txt").split()
+grupo_teste = [int(IDS[0])]
+mat_discreta = [int(IDS[1])]
+icc = [int(IDS[2])]
+fisica = [int(IDS[3]), int(IDS[4])]
+algoritmos = [int(IDS[4]), int(IDS[5])]
+grupos_sala = [int(IDS[5]), int(IDS[6])]
+grupos_permitidos = [grupo_teste, grupos_sala[0], grupos_sala[1]]
--- main.py
+++ main.py
@@ -19,16 +19,6 @@ from globais import *
 
 #version 6.4.1
 
-# IDS = loadFile("Ignore/Groups.txt").split()
-'''
-grupo_teste = [int(IDS[0])]
-mat_discreta = [int(IDS[1])]
-icc = [int(IDS[2])]
-fisica = [int(IDS[3]), int(IDS[4])]
-algoritmos = [int(IDS[4]), int(IDS[5])]
-grupos_sala = [int(IDS[5]), int(IDS[6])]
-grupos_permitidos = [grupo_teste, grupos_sala[0], grupos_sala[1]]
-'''
 
 def main():
     TOKEN = os.getenv("TELEGRAM_TOKEN")
